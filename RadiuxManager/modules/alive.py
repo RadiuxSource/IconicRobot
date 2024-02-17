946d9ef18f641ca6c9c94f97653617262cdb6414
AdityaJrsu17 <71754876+Ruchi0406@users.noreply.github.com>
2024-02-17 11:32:18 +0530

    Update alive.py

--- RadiuxManager/modules/alive.py
+++ RadiuxManager/modules/alive.py
@@ -66,11 +66,11 @@ async def restart(client, m: Message):
     await asyncio.sleep(0.2)
     await m.reply_photo(
         random.choice(MISHI),
-        caption=f"""** ✦ ʜᴇʏ, ɪ ᴀᴍ [{BOT_NAME}](f"t.me/{BOT_USERNAME}") ✦**\n\n➩ **ʟɪʙʀᴀʀʏ ➛** `{lver}`\n➩ **ᴛᴇʟᴇᴛʜᴏɴ ➛** `{tver}`\n➩ **ᴘʏʀᴏɢʀᴀᴍ ➛** `{pver}`\n➩ **ᴘʏᴛʜᴏɴ ➛** `{pyver()}`\n\n➩ **ᴍᴀᴅᴇ ʙʏ ➛** [ʀᴏʏ-ᴇᴅɪᴛx](tg://user?id={OWNER_ID})""",
+        caption=f"""** ✦ ʜᴇʏ, ɪ ᴀᴍ [{BOT_NAME}](f"t.me/{BOT_USERNAME}") ✦**\n\n➩ **ʟɪʙʀᴀʀʏ ➛** `{lver}`\n➩ **ᴛᴇʟᴇᴛʜᴏɴ ➛** `{tver}`\n➩ **ᴘʏʀᴏɢʀᴀᴍ ➛** `{pver}`\n➩ **ᴘʏᴛʜᴏɴ ➛** `{pyver()}`\n\n➩ **ᴍᴀᴅᴇ ʙʏ ➛** [𝐑𝙰𝙳𝙸𝚄𝚇](tg://user?id={OWNER_ID})""",
         reply_markup=InlineKeyboardMarkup(Radiux),
     )
     
-__mod_name__ = "ᴀʟɪᴠᴇ"
+__mod_name__ = "𝐀𝙻𝙸𝚅𝙴"
 __help__ = """
  ➩ /alive ➛ ᴄʜᴇᴄᴋ ʙᴏᴛ ᴀʟɪᴠᴇ sᴛᴀᴛᴜs.
  ➩ /ping ➛ ᴄʜᴋ ᴘɪɴɢ sᴛᴀᴛᴜs.
