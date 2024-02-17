2e95e95ff7235aefcd30ee0b6f89629cba676aba
AdityaJrsu17 <71754876+Ruchi0406@users.noreply.github.com>
2024-02-17 11:36:21 +0530

    Update animez.py

--- RadiuxManager/modules/animez.py
+++ RadiuxManager/modules/animez.py
@@ -573,7 +573,7 @@ dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
 
-__mod_name__ = "ᴀɴɪᴍᴇ"
+__mod_name__ = "𝐀𝙽𝙸𝙼𝙴𝚂"
 __command_list__ = [
     "anime",
     "manga",
