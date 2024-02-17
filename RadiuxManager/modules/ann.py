581503332923e5b38bc107e2cf0e893dc1deb85f
AdityaJrsu17 <71754876+Ruchi0406@users.noreply.github.com>
2024-02-17 11:37:09 +0530

    Update ann.py

--- RadiuxManager/modules/ann.py
+++ RadiuxManager/modules/ann.py
@@ -66,7 +66,7 @@ ANN_HANDLER = CommandHandler('ann', ann, run_async=True)
 dispatcher.add_handler(ANN_HANDLER)
 
 #  Buttons for /help .
-__mod_name__ = "ᴀɴɪᴍᴇ-ɴ"  # *IF YOU WANT* EDIT NAME OF BUTTON IN '/help'
+__mod_name__ = "𝐀𝙽𝙸𝙼𝙴-𝐍"  # *IF YOU WANT* EDIT NAME OF BUTTON IN '/help'
 
 # *IF YOU WANT* EDIT MESSAGE FOR HELP OF THIS MODULE.
 __help__ = '''
