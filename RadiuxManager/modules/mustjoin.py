5f2d82dd1bd4cd9182b5df2dc8c177ff39548149
AdityaJrsu17 <71754876+Ruchi0406@users.noreply.github.com>
2024-02-17 15:26:15 +0530

    Update mustjoin.py

--- RadiuxManager/modules/mustjoin.py
+++ RadiuxManager/modules/mustjoin.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from Equinox import Equinox as app
+from RadiuxManager import pbot as app
 
 #--------------------------
 
