04ca14f4e34d559b85b7625becf748f2884ef0b6
AdityaJrsu17 <71754876+Ruchi0406@users.noreply.github.com>
2024-02-17 16:37:20 +0530

    Update blackpink.py

--- RadiuxManager/modules/blackpink.py
+++ RadiuxManager/modules/blackpink.py
@@ -9,3 +9,6 @@ async def blackpink(_, message):
     bp(f"{text}").save(f"blackpink_{message.from_user.id}.png")
     await message.reply_photo(f"blackpink_{message.from_user.id}.png")
     os.remove(f"blackpink_{message.from_user.id}.png")
+
+__mod_name__ = "𝐁𝙻𝙰𝙲𝙺𝙿𝙸𝙽𝙺"
+__help__ = """➔ /blackpink <text>"""
