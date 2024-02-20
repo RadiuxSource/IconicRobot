241a7019371cef9d56a1c220f3d59c39b97ba5ac
AdityaJrsu17 <71754876+Ruchi0406@users.noreply.github.com>
2024-02-20 07:46:01 +0530

    Update speed_test.py

--- RadiuxManager/modules/speed_test.py
+++ RadiuxManager/modules/speed_test.py
@@ -64,7 +64,7 @@ __help__ = """
 ➩ /speedtest *➛* ʀᴜɴs ᴀ sᴘᴇᴇᴅᴛᴇsᴛ ᴀɴᴅ ᴄʜᴇᴄᴋ ᴛʜᴇ sᴇʀᴠᴇʀ sᴘᴇᴇᴅ.
 """
 
-__mod_name__ = "s-ᴛᴇsᴛ"
+__mod_name__ = "𝐒-𝚃𝙴𝚂𝚃"
 
 __command_list__ = ["speedtest"]
 __handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
