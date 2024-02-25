d9f1bdd7c4ba4219a991144d0ed06044ac89cb9e
RadiuxSource <157123399+RadiuxSource@users.noreply.github.com>
2024-02-25 14:42:17 +0530

    Update ptag.py

--- RadiuxManager/modules/ptag.py
+++ RadiuxManager/modules/ptag.py
@@ -18,7 +18,7 @@ async def ptag(event):
 
     # Check if the user is an admin or the creator of the chat
     try:
-        participant = await client(GetParticipantRequest(channel=event.chat_id, user_id=event.sender_id))
+        participant = await client(GetParticipantRequest(channel=event.chat_id, participant_id=event.sender_id))
         user_permissions = participant.participant
         if not isinstance(user_permissions, (ChannelParticipantAdmin, ChannelParticipantCreator)):
             return await event.respond("__You need to be an admin to use this command.__")
@@ -71,4 +71,3 @@ __help__ = """
 - /ptag <message>: Broadcast a message to all members of the group or channel personally. Admin only.
 - /cancel: Cancel the ongoing /ptag process.
 """
-      
